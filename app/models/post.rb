05bed06f6de9d5378243ace2b971b173be41a82f
Jenny <jenny.kats@aol.com>
2017-01-02 10:57:51 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validate :clickbait?
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+  message: "%{value} is not a valid category" }
+end
+
+
+def clickbait?
+  unless !title.blank? && title.include?("Won't Believe"|| "Secret" || "Top
+    [number]" || "Guess")
+  errors.add(:title, "not clickbait")
+end
+
+
 end
